10230b4e4201818fe3c99c6b6b2b0092d2c3c3e5
sark1 <rockstarsubhajit1999@gmail.com>
2024-01-16 21:56:09 +0530

    changes

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import reducers from "./Redux/Reducer/RootReducer";
-import {thunk} from "redux-thunk";
-
-const store = createStore(reducers, applyMiddleware(thunk));
-
-export default store;
