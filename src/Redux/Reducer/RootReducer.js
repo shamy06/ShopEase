3f9ac0a445f039dc78d8ac6c7ffbd3fc65fca0d8
sark1 <rockstarsubhajit1999@gmail.com>
2024-01-16 22:02:24 +0530

    changes

--- src/Redux/Reducer/RootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { productsReducer } from "./ProductReducer";
-
-const reducers = combineReducers({
-  allProducts: productsReducer,
-});
-
-export default reducers;
